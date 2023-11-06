63555ca492b73527052089afe36efd0fa2d67601
Pawel <pawel@vps-313d5111.vps.ovh.net>
2023-11-06 19:56:00 +0000

    Dummy test commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import torch
+
+def main():
+    print("Hello PyTorchTest")
+
+    x = torch.rand(5, 3)
+
+    print(x)
+
+    print("Goodbye PyTorchTest");
+
+if __name__ == "__main__":
+    main()
